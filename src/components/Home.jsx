747f8cf17a371b3cb9d617bfcb61c21bea5d1cce
tapas-code <tapas.code247@gmail.com>
2023-04-18 14:12:08 +0530

    Added profile option

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="https://wallpapercave.com/wp/wp1987043.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+            <img src="https://www.harvestify.in/images/sustainability/1.jpg" className="max-w-xl rounded-lg shadow-2xl" />
             <div>
             <h1 className="text-5xl font-bold">App for farmers</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <p className="py-6">Plant diseases are a severe threat to the entire production. Therefore, it is essential for farmers to effectively deal with them and check them with the help of timely prevention.</p>
             <button className="btn btn-primary">Get Started</button>
             </div>
         </div>
